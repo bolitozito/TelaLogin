196c00d289f4acc566a40f4f7ede8389fb9c4fed
bolitozito <amendoinsanda@gmail.com>
2026-05-23 16:47:12 -0300

    23-05

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const container = document.querySelector('.container');
-const register = document.querySelector('.register-btn');
+const registerBtn = document.querySelector('.register-btn');
+const loginBtn = document.querySelector('.login-btn');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add('active');
+})
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove('active');
+})
